fb242fbfba92be736acfe46c58e1bc02979ed745
Nikita Kisly <sour945@gmail.com>
2023-05-13 15:16:47 +0300

    feat: add google fonts

--- src/features/Navbar/ui/Navbar.tsx
+++ src/features/Navbar/ui/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar: FC = () => {
   return (
     <Group spacing={60} sx={{ flex: '1 1 100%', flexWrap: 'nowrap', justifyContent: 'center' }}>
       <NavLink fw={500} label="Поиск вакансий" sx={{ width: 'auto' }} />
-      <NavLink fw={400} label="Избранное" sx={{ width: 'auto' }} />
+      <NavLink fw={500} label="Избранное" sx={{ width: 'auto' }} />
     </Group>
   );
 };
