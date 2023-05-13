bdd32cd41a78191fdfbdd2ea995161b2e8c0a3c0
Nikita Kisly <sour945@gmail.com>
2023-05-13 14:57:44 +0300

    feat: add authorization side effect for app

--- src/processes/Layout/Layout.tsx
+++ src/processes/Layout/Layout.tsx
@@ -1,12 +1,22 @@
-import { FC } from 'react';
+import { FC, Suspense, useContext, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'widgets';
+import { StateContext, useGetAccessTokenQuery } from 'shared';
 
 export const Layout: FC = () => {
+  const { data, isLoading, isSuccess } = useGetAccessTokenQuery({});
+  const { localStorage } = useContext(StateContext);
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      localStorage.setItem('access_token', data.access_token);
+    }
+  }, [isSuccess, data, localStorage]);
+
   return (
     <section>
       <Header />
-      <Outlet />
+      <Suspense fallback="loading...">{isLoading ? 'loading...' : <Outlet />}</Suspense>
     </section>
   );
 };
