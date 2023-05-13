fbfd52c578b5e3d6610a790ba8a205d940b08315
Nikita Kisly <sour945@gmail.com>
2023-05-13 14:53:40 +0300

    feat: update mantine theme

--- src/app/providers/with-mantine.tsx
+++ src/app/providers/with-mantine.tsx
@@ -1,8 +1,25 @@
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, MantineThemeOverride } from '@mantine/core';
+
+const theme: MantineThemeOverride = {
+  fontFamily: 'Inter, sans-serif',
+  lineHeight: '20px',
+  primaryColor: 'blue',
+  loader: 'dots',
+  fontSizes: {
+    xl: '28px',
+    lg: '24px',
+    md: '20px',
+    sm: '16px',
+    xs: '14px'
+  },
+  colors: {
+    white: ['#FFFFFF']
+  }
+};
 
 export const withMantine = (component: () => React.ReactNode) => () =>
   (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
       {component()}
     </MantineProvider>
   );
