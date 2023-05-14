d27729e0205c6148ba73477b25d2e03d4f0175e4
Nikita Kisly <sour945@gmail.com>
2023-05-14 20:44:37 +0300

    feat: add routing for header

--- src/entities/LogoGroup/ui/LogoGroup.tsx
+++ src/entities/LogoGroup/ui/LogoGroup.tsx
@@ -1,21 +1,24 @@
 import { FC } from 'react';
 import { Box, Text, Image } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
 import logo from './assets/logo.png';
 
 export const LogoGroup: FC = () => {
   return (
-    <Box
-      sx={(theme) => ({
-        display: 'flex',
-        alignItems: 'center',
-        gap: theme.spacing.sm,
-        height: '100%'
-      })}
-    >
-      <Image height={30} width={30} src={logo} />
-      <Text size="lg" fw={600} ff="Poppins, sans-serif">
-        Jobored
-      </Text>
-    </Box>
+    <NavLink to={'/'}>
+      <Box
+        sx={(theme) => ({
+          display: 'flex',
+          alignItems: 'center',
+          gap: theme.spacing.sm,
+          height: '100%'
+        })}
+      >
+        <Image height={30} width={30} src={logo} />
+        <Text size="lg" fw={600} ff="Poppins, sans-serif">
+          Jobored
+        </Text>
+      </Box>
+    </NavLink>
   );
 };
--- src/features/Navbar/ui/Navbar.tsx
+++ src/features/Navbar/ui/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Group, NavLink } from '@mantine/core';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Navbar: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <Group spacing={60} sx={{ flex: '1 1 100%', flexWrap: 'nowrap', justifyContent: 'center' }}>
-      <NavLink fw={500} label="Поиск вакансий" sx={{ width: 'auto' }} />
-      <NavLink fw={500} label="Избранное" sx={{ width: 'auto' }} />
+      <NavLink fw={500} label="Поиск вакансий" sx={{ width: 'auto' }} onClick={() => navigate('/')} />
+      <NavLink fw={500} label="Избранное" sx={{ width: 'auto' }} onClick={() => navigate('/favorites')} />
     </Group>
   );
 };
