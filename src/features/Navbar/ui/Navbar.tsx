83beb01cf10f7e66501fff47d7a761c5ea9534da
Nikita Kisly <sour945@gmail.com>
2023-05-15 16:28:22 +0300

    refactor: change nav routes

--- src/features/Navbar/ui/Navbar.tsx
+++ src/features/Navbar/ui/Navbar.tsx
@@ -1,14 +1,15 @@
 import { Group, NavLink } from '@mantine/core';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'shared';
 
 export const Navbar: FC = () => {
   const navigate = useNavigate();
 
   return (
     <Group spacing={60} sx={{ flex: '1 1 100%', flexWrap: 'nowrap', justifyContent: 'center' }}>
-      <NavLink fw={500} label="Поиск вакансий" sx={{ width: 'auto' }} onClick={() => navigate('/')} />
-      <NavLink fw={500} label="Избранное" sx={{ width: 'auto' }} onClick={() => navigate('/favorites')} />
+      <NavLink fw={500} label="Поиск вакансий" sx={{ width: 'auto' }} onClick={() => navigate(ROUTES.main)} />
+      <NavLink fw={500} label="Избранное" sx={{ width: 'auto' }} onClick={() => navigate(ROUTES.favorites)} />
     </Group>
   );
 };
